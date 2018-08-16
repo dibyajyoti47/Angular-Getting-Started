e142465b55ec3f75ff25d3f031f5ce1d15a603b1
Dibyajyoti Behera <dibyajyoti.a.behera@accenture.com>
2018-08-16 19:41:33 +0530

    module 13 completed

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from 'src/app/products/product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,9 +12,20 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private activatedRoute : ActivatedRoute,
+              private router : Router,
+              private productService : ProductService) { }
 
   ngOnInit() {
+    let id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.pageTitle += ` : ${id}`;
+    this.productService.getProducts().subscribe((products) => {
+      this.product=products[id];
+      console.log(this.product);
+      })
+    }
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
---
+++ src/app/products/product.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductDetailGuard } from './product-detail.guard';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: 'products', component: ProductListComponent },
+      { path: 'products/:id', component: ProductDetailComponent, canActivate: [ProductDetailGuard] }
+    ]),
+    SharedModule
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent
+  ]
+})
+export class ProductModule { }
